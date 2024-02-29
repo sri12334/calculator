e9c48c8c27a44146cb017d7e5f94ef328d661246
sri12334 <srir48747@gmail.com>
2024-02-29 14:45:54 +0100

    calculator is done

--- src/index.js
+++ src/index.js
@@ -1,2 +1,48 @@
+/* eslint-disable no-restricted-globals */
 // Say hello let'us build a calculator
 alert("Hello there, let'us build a calculator");
+let num1;
+let num2;
+let isValid = false;
+while (!isValid) {
+  num1 = prompt('please entre your first number');
+  num2 = prompt('please entre your second number');
+  if (!isNaN(num1) || !isNaN(num2)) {
+    isValid = true; // Break out of the loop if both inputs are valid numbers
+  } else {
+    alert('Please enter valid numbers.');
+  }
+}
+let operator;
+isValid = false;
+while (!isValid) {
+  operator = prompt('please choose one of those (+, -, /, %, *, **)');
+
+  if (['+', '-', '/', '%', '*', '**'].includes(operator)) {
+    isValid = true;
+  } else {
+    alert('Please enter valid operator.');
+  }
+}
+
+let result = '';
+
+if (operator === '+') {
+  result = num1 + num2;
+} else if (operator === '-') {
+  result = num1 - num2;
+} else if (operator === '') {
+  result = num1 - num2;
+} else if (operator === '/') {
+  result = num1 / num2;
+} else if (operator === '%') {
+  result = num1 % num2;
+} else if (operator === '*') {
+  result = num1 * num2;
+} else if (operator === '**') {
+  result = num1 ** num2;
+} else {
+  alert('invalid operator');
+}
+
+alert(`result: ${result}`);
